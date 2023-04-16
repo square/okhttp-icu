d792bb98a6ba69234b499bb66b51ed0ce0de4cf6
Jesse Wilson <jwilson@squareup.com>
2023-04-16 13:00:27 -0400

    Isolate the size threshold

--- okhttp-icu/src/windowsTest/kotlin/com/squareup/okhttpicu/MaxExpansionWindowsTest.kt
+++ okhttp-icu/src/windowsTest/kotlin/com/squareup/okhttpicu/MaxExpansionWindowsTest.kt
@@ -39,25 +39,11 @@ class MaxExpansionWindowsTest {
 
   @Test
   fun maxExpansion() {
-    val a = "a"
-    val b = "(ﷺ)"
-    val c = "(صلى الله عليه وسلم)"
-    val a100 = a.repeat(100)
-    val b100 = b.repeat(100)
-    val c100 = c.repeat(100)
-    val a2048 = a.repeat(2048)
-    val b2048 = b.repeat(2048)
-    val c2048 = c.repeat(2048)
-    assertEquals(a100, a100.normalize(NormalizationKD))
-    assertEquals(c100, b100.normalize(NormalizationKD))
-    assertEquals(c100, c100.normalize(NormalizationKD))
-    assertEquals(a100 + c, (a100 + c).normalize(NormalizationKD))
-    assertEquals(a100 + c, (a100 + b).normalize(NormalizationKD))
-    assertEquals(a100 + c100, (a100 + c100).normalize(NormalizationKD))
-    assertEquals(a100 + c100, (a100 + b100).normalize(NormalizationKD))
-    assertEquals(a2048 + c, (a2048 + c).normalize(NormalizationKD))
-    assertEquals(a2048 + c, (a2048 + b).normalize(NormalizationKD))
-    assertEquals(a2048 + c2048, (a2048 + c2048).normalize(NormalizationKD))
-    assertEquals(a2048 + c2048, (a2048 + b2048).normalize(NormalizationKD))
+    val a = "(ﷺ)"
+    val b = "(صلى الله عليه وسلم)"
+    for (i in 0 until 100) {
+      println(i)
+      assertEquals(b.repeat(i), a.repeat(i).normalize(NormalizationKD))
+    }
   }
 }
